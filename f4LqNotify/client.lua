9fc240ccbfca7bc17d15b01013e7af1cb9e23f30
f4Lq <167790412+f4Lq@users.noreply.github.com>
2024-05-26 13:33:29 +0200

    Update client.lua

--- f4LqNotify/client.lua
+++ f4LqNotify/client.lua
@@ -39,6 +39,14 @@ RegisterCommand('notify6', function()
 	exports['f4LqNotify']:Alert("SAVED", "<span style='color:#c7c7c7'>Ubrania zapisane pomyślnie!", 5000, 'long')
 end)
 
+RegisterCommand('notify7', function()
+	exports['f4LqNotify']:Alert("OGŁOSZENIE", "<span style='color:#c7c7c7'>Administarcyjne ogłoszenie!", 5000, 'admin')
+end)
+
+RegisterCommand('notify8', function()
+	exports['f4LqNotify']:Alert("OGŁOSZENIE", "<span style='color:#c7c7c7'>Informacje powiązane z Pojazdami!", 5000, 'car')
+end)
+
 ---[ ALL IN ONE NOTIFY (DELETE THIS BELOW) ]---
 
 RegisterCommand('allnotify', function()
@@ -48,4 +56,6 @@ RegisterCommand('allnotify', function()
 	exports['f4LqNotify']:Alert("NEW SMS", "<span style='color:#ffc107'>695-2713: </span><span style='color:#c7c7c7'> Jak się masz?", 5000, 'warning')
 	exports['f4LqNotify']:Alert("TWITTER", "<span style='color:#01a2dc'>@USER69: </span><span style='color:#c7c7c7'> Witam wszystkich!", 5000, 'sms')
 	exports['f4LqNotify']:Alert("SAVED", "<span style='color:#c7c7c7'>Ubrania zapisane pomyślnie!", 5000, 'long')
+	exports['f4LqNotify']:Alert("OGŁOSZENIE", "<span style='color:#c7c7c7'>Administarcyjne ogłoszenie!", 5000, 'admin')
+	exports['f4LqNotify']:Alert("OGŁOSZENIE", "<span style='color:#c7c7c7'>Informacje powiązane z Pojazdami!", 5000, 'car')
 end)
